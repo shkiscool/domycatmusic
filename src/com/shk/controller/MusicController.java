d35e602fd5b3f1e7e7ab9552b4eb29efc4ab678d
阿瓜 <Administrator@Stu1001>
2018-06-08 16:34:28 +0800

    tttt

--- src/com/shk/controller/MusicController.java
+++ src/com/shk/controller/MusicController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import com.google.gson.Gson;
 import com.shk.entity.Music;
 import com.shk.entity.Song;
+import com.shk.entity.View_Singer;
 import com.shk.service.MusicService;
 import com.shk.serviceImpl.MusicServiceImpl;
 import com.shk.util.PageData;
@@ -24,99 +25,99 @@ import com.shk.util.PageData;
 @WebServlet("/MusicController")
 public class MusicController extends HttpServlet {
 	static MusicService ms = new MusicServiceImpl();
-	
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MusicController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public MusicController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//基本编码设置；
+		// 基本编码设置；
 		response.setContentType("text/html; charset=utf-8");
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
 		HttpSession session = request.getSession();
-		
+
 		String op = "query";
 		int page = 1;
-		int pageSize = 1;
+		int pageSize = 12;
 		String musicLike = "";
-		
-		if(null!=request.getParameter("op"))
-		{
-			//获取op的值
+
+		if (null != request.getParameter("op")) {
+			// 获取op的值
 			op = request.getParameter("op");
 		}
 
-		if("query".equals(op)) {
-			
-			if(null!=request.getParameter("page"))
-			{
-				page =Integer.parseInt(request.getParameter("page"));
+		if ("query".equals(op)) {
+
+			if (null != request.getParameter("page")) {
+				page = Integer.parseInt(request.getParameter("page"));
 			}
-			if(null!=request.getParameter("pageSize"))
-			{
-				pageSize =Integer.parseInt(request.getParameter("pageSize"));
+			if (null != request.getParameter("pageSize")) {
+				pageSize = Integer.parseInt(request.getParameter("pageSize"));
 			}
-			if(null!=request.getParameter("musicLike"))
-			{
+			if (null != request.getParameter("musicLike")) {
 				musicLike = request.getParameter("musicLike");
 			}
-			
-		
-			if(null!=request.getParameter("musicId")) {
-				
-				int musicId =Integer.parseInt(request.getParameter("musicId"));
-				
-				List<Song> songList = (List<Song>)session.getAttribute("songList");
-				String str = addSongToMyList(session,request, response, musicId, songList);
-				System.out.println(str);
+
+			if (null != request.getParameter("musicId")) {
+
+				int musicId = Integer.parseInt(request.getParameter("musicId"));
+
+				List<Song> songList = (List<Song>) session.getAttribute("songList");
+				String str = addSongToMyList(session, request, response, musicId, songList,"musicId");
 				request.setAttribute("autoPlay", true);
 				request.setAttribute("data", str);
+			} else if (null != request.getParameter("addMusicId")) {
+				int addMusicId = Integer.parseInt(request.getParameter("addMusicId"));
+
+				List<Song> songList = (List<Song>) session.getAttribute("songList");
+				String str = addSongToMyList(session, request, response, addMusicId, songList,"addMusicId");
+				request.setAttribute("autoPlay", false);
+				request.setAttribute("data", str);
+
 			} else {
 				
-				List<Song> songList = getMyList(page, pageSize, musicLike);
+				List<Song> songList = getMyList(page, 1, musicLike);
 				String str = changToStr(songList);//我的音乐列表
 				session.setAttribute("songList", songList);
-				System.out.println("songList:"+str);
 				request.setAttribute("autoPlay", false);
 				request.setAttribute("data", str);
 			}
-			
-			
-			PageData<Music> pdRandom = getRandomMusic(2,12);//推荐音乐
-			PageData<Music> pdNew = getRandomMusic(1,8);//新曲推荐
+
+			PageData<Music> pdRandom = getRandomMusic(2, 12);// 推荐音乐
+			PageData<Music> pdNew = getRandomMusic(1, 8);// 新曲推荐
 			request.setAttribute("pdNew", pdNew);
-			
+
 			request.setAttribute("pdRandom", pdRandom);
-			
+
 			request.getRequestDispatcher("jsps/NewFile.jsp").forward(request, response);
 		}
-		
+
 		/**
 		 * 后续添加
 		 */
 		/**
 		 * 勾思奇：后续添加搜索得到分页歌单列表的方法
 		 */
-		else if("search".equals(op)) {
-			if(null!=request.getParameter("page"))
-			{
-				page =Integer.parseInt(request.getParameter("page"));
+		else if ("search".equals(op)) {
+			if (null != request.getParameter("page")) {
+				page = Integer.parseInt(request.getParameter("page"));
 			}
-			if(null!=request.getParameter("pageSize"))
-			{
-				pageSize =Integer.parseInt(request.getParameter("pageSize"));
+			if (null != request.getParameter("pageSize")) {
+				pageSize = Integer.parseInt(request.getParameter("pageSize"));
 			}
 			String keyword = request.getParameter("keyword");
 			PageData<Music> pd = ms.getMusic(page, pageSize, keyword);
@@ -124,53 +125,84 @@ public class MusicController extends HttpServlet {
 			String data = gson.toJson(pd);
 			out.print(data);
 		}
+
+		/**
+		 * 获取歌手列表
+		 */
+		else if("showSinger".equals(op)) {
+			
+			List<View_Singer> singerList = ms.getAllSinger();
+			
+			Gson gson = new Gson();
+			String data = gson.toJson(singerList);
+			out.print(data);
+		}
+
+		
+		/**
+		 * 获取歌手列表
+		 */
+		else if("showSinger".equals(op)) {
+			
+			List<View_Singer> singerList = ms.getAllSinger();
+			
+			Gson gson = new Gson();
+			String data = gson.toJson(singerList);
+			out.print(data);
+		}
+
 		
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		
+
 	}
-	
+
 	/**
 	 * 获得随机的推荐歌曲
+	 * 
 	 * @param pageSize
 	 */
-	public static PageData<Music> getRandomMusic(int page,int pageSize) {
-//		Random rd = new Random();
-//		int page = rd.nextInt(5);
-		
+	public static PageData<Music> getRandomMusic(int page, int pageSize) {
+		// Random rd = new Random();
+		// int page = rd.nextInt(5);
+
 		PageData<Music> pd = ms.getMusic(page, pageSize, "");
-		
+
 		return pd;
-		
+
 	}
-	
+
 	/**
 	 * 获得我的音乐列表的歌曲
+	 * 
 	 * @param page
 	 * @param pageSize
 	 * @param musicLike
 	 * @return
 	 */
-	public static List<Song> getMyList(int page,int pageSize,String musicLike) {
+	public static List<Song> getMyList(int page, int pageSize, String musicLike) {
 		PageData<Music> pd = ms.getMusic(page, pageSize, musicLike);
 		List<Song> songList = ms.changeToSong(pd.getData());
-		
+
 		return songList;
 	}
-	
+
 	/**
 	 * 将List转换为Jplayer可接受的字符串格式
+	 * 
 	 * @param songList
 	 * @return
 	 */
 	public static String changToStr(List<Song> songList) {
-		if(songList==null) {
+		if (songList == null) {
 			return null;
 		}
 		StringBuilder sb = new StringBuilder();
@@ -178,28 +210,29 @@ public class MusicController extends HttpServlet {
 		for (int i = 0; i < songList.size(); i++) {
 			sb.append("{");
 			sb.append(songList.get(i).toString());
-			if(i==(songList.size()-1)) {
+			if (i == (songList.size() - 1)) {
 				sb.append("}");
 			} else {
-				
+
 				sb.append("},");
 			}
 		}
 		sb.append("]");
 		String str = new String(sb);
-		
+
 		return str;
 	}
-	
-	public static String addSongToMyList(HttpSession session,HttpServletRequest request, HttpServletResponse response,int musicId,List<Song> songList) {
-		System.out.println(request.getParameter("musicId"));
-		int id =Integer.parseInt(request.getParameter("musicId"));
+
+	public static String addSongToMyList(HttpSession session, HttpServletRequest request, HttpServletResponse response,
+			int musicId, List<Song> songList,String key) {
+		System.out.println(request.getParameter(key));
+		int id = Integer.parseInt(request.getParameter(key));
 		Music music = ms.getMusicById(id);
 		Song song = new Song(music.getmName(), music.getmUrl(), music.getmImg(), music.getSingerName());
-		if(null!=songList) {
-			songList.add(0, song);				
+		if (null != songList) {
+			songList.add(0, song);
 		}
-		
+
 		session.setAttribute("songList", songList);
 		String str = changToStr(songList);
 		return str;
